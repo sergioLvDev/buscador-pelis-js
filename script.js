773d9e22be33a0f9ecc1328dab7f7456d6226b65
slavilletta <sergio_pk@hotmail.com>
2024-12-04 20:10:08 -0300

    buscador de peliculas

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let btnBuscar = document.getElementById("searchButton");
 let mostrarPelis = document.getElementById("results");
-console.log(btnBuscar);
-console.log(mostrarPelis);
 
 btnBuscar.addEventListener("click", () => {
   let input = document.getElementById("searchInput").value;
@@ -15,13 +13,19 @@ btnBuscar.addEventListener("click", () => {
     .catch((error) => console.error(error));
 });
 function mostrarDatos(data) {
-  console.log(data.results);
-}
-
-/* 
+  mostrarPelis.innerHTML = ""; ////Borra la lista si es que ya hay peliculas por cada vez que se apriete el boton
   data.results.forEach((element) => {
     let titulo = element.title;
-    console.log(titulo);
-    mostrarPelis.classList.add("movie");
-    mostrarPelis.innerHTML = `<h2>${titulo}</2>`;
-  }); */
+    let descripcion = element.overview;
+    let poster = element.poster_path;
+    mostrarPelis.innerHTML += `<div class="movie">
+                               <img src="https://image.tmdb.org/t/p/w200/${poster} alt="${titulo}"> 
+                               <h2>${titulo}</h2>
+                               <p>${descripcion}</p>
+                               </div>`;
+  });
+}
+
+//1er paso traigo los elementos del DOM // id de los elementos
+//2do paso traigo la info de la Api // con el fetch //trae un objeto con un array adentro de todas las coincidencias
+//3er paso creo la funcion para buscar // obtengo la info de cada elemento del array y genero una tarjeta para cada pelicula con el acumulador += para que se agreguen las peliculas una abajo de la otra
